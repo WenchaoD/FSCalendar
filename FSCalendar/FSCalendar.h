b3fe6548507de673bd97046587f7cf26dfbe5a17
f33chobits <=>
2015-08-23 18:32:47 +0800

    FSCalendarScopeWeek is coming soon
    FSCalendarScopeWeek is coming soon

--- FSCalendar/FSCalendar.h
+++ FSCalendar/FSCalendar.h
@@ -17,6 +17,7 @@ typedef NS_ENUM(NSInteger, FSCalendarFlow) {
     FSCalendarFlowHorizontal
 };
 
+/** Comming soon */
 typedef NS_ENUM(NSInteger, FSCalendarScope) {
     FSCalendarScopeMonth,
     FSCalendarScopeWeek
