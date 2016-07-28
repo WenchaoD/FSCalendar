057d2695b0f6f47de650d4333deaa2a792404b7b
Abner <fjhongdong@126.com>
2016-07-28 17:44:45 +0800

    add HDDemo

---
+++ Example/HDReservationManagerVC.h
@@ -0,0 +1,13 @@
+//
+//  HDReservationManagerVC.h
+//  ihealth4d
+//
+//  Created by 洪东 on 5/24/16.
+//  Copyright © 2016 abnerh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDReservationManagerVC : UIViewController
+
+@end
