bfa58ebe4a0bd21a2c2439131e1724c00117124c
Wenchao Ding <f33chobits@gmail.com>
2016-08-24 14:46:13 +0800

    Add test
    Add test

--- Example/FSCalendarTests/FSCalendarTests.m
+++ Example/FSCalendarTests/FSCalendarTests.m
@@ -2,14 +2,19 @@
 //  FSCalendarTests.m
 //  FSCalendarTests
 //
-//  Created by Wenchao Ding on 8/23/15.
-//  Copyright (c) 2015 wenchaoios. All rights reserved.
+//  Created by dingwenchao on 8/24/16.
+//  Copyright © 2016 wenchaoios. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "FSCalendar.h"
+#import "FSCalendarDynamicHeader.h"
 
 @interface FSCalendarTests : XCTestCase
+
+@property (strong, nonatomic) FSCalendar *calendar;
+@property (strong, nonatomic) NSDate *date;
+@property (strong, nonatomic) NSIndexPath *indexPath;
 
 @end
 
@@ -18,6 +23,9 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.calendar = [[FSCalendar alloc] initWithFrame:CGRectMake(0, 0, 320, 300)];
+    self.indexPath = [NSIndexPath indexPathForItem:25 inSection:0];
+    self.date = [self.calendar dateForIndexPath:self.indexPath];
 }
 
 - (void)tearDown {
@@ -27,13 +35,18 @@
 
 - (void)testExample {
     // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
+- (void)testIndexPathForDatePerformance {
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        [self.calendar indexPathForDate:self.date];
+    }];
+}
+
+- (void)testDateForIndexPathPerformance {
+    [self measureBlock:^{
+        [self.calendar dateForIndexPath:self.indexPath];
     }];
 }
 
