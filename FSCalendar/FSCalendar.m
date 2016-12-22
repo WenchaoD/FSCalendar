841ab95c6cfe1cc1f2ca8f3bec509f7057eb5454
Oleg Sorochich <sorochich.oleg@intellectsoft.net>
2016-12-22 20:17:58 +0300

    Set placeholder flag correctly for the week calendar scope

--- FSCalendar/FSCalendar.m
+++ FSCalendar/FSCalendar.m
@@ -1605,7 +1605,7 @@ typedef NS_ENUM(NSUInteger, FSCalendarOrientation) {
             break;
         }
         case FSCalendarScopeWeek: {
-            cell.placeholder = ![self isDateInRange:date];
+            cell.placeholder = (cell.monthPosition == FSCalendarMonthPositionPrevious || cell.monthPosition == FSCalendarMonthPositionNext) || ![self isDateInRange:date];
             break;
         }
     }
