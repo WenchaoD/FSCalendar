a7c0f50422dbcd0bb0ff0de3fe94b4764e7d39dd
丁文超 <f33chobits@gmail.com>
2017-08-08 09:10:09 +0800

    Fix a wrong bis-check.

--- Example-Swift/FSCalendarSwiftExample/LunarFormatter.swift
+++ Example-Swift/FSCalendarSwiftExample/LunarFormatter.swift
@@ -13,7 +13,7 @@ open class LunarFormatter: NSObject {
     fileprivate let chineseCalendar = Calendar(identifier: .chinese)
     fileprivate let formatter = DateFormatter()
     fileprivate let lunarDays = ["初二","初三","初四","初五","初六","初七","初八","初九","初十","十一","十二","十三","十四","十五","十六","十七","十八","十九","二十","二一","二二","二三","二四","二五","二六","二七","二八","二九","三十"]
-    fileprivate let lunarMonths = ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","冬月","腊月"]
+    fileprivate let lunarMonths = ["正月","二月","三月","四月","五月","六月","七月","八月","九月","十月","冬月","腊月"]
     
     
     override init() {
@@ -28,7 +28,7 @@ open class LunarFormatter: NSObject {
         }
         // First day of month
         let monthString = self.formatter.string(from: date)
-        if !monthString.contains("bis") {
+        if self.chineseCalendar.veryShortMonthSymbols.contains(monthString) {
             if let month = Int(monthString) {
                 return self.lunarMonths[month-1]
             }
