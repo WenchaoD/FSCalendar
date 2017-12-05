90b66d047c826dfb4cba0b910e2bb83a8ce0e83b
vasanthdev <vasanth@twilightsoftwares.com>
2017-12-06 00:25:36 +0530

    Update FSCalendar.m

--- FSCalendar/FSCalendar.m
+++ FSCalendar/FSCalendar.m
@@ -995,7 +995,7 @@ typedef NS_ENUM(NSUInteger, FSCalendarOrientation) {
         CGFloat contentHeight = self.transitionCoordinator.cachedMonthSize.height-headerHeight-weekdayHeight-_scopeHandle.fs_height;
         CGFloat padding = 5;
         if (!self.floatingMode) {
-            _preferredRowHeight = (contentHeight-padding*2)/6.0;
+            _preferredRowHeight = (contentHeight-padding*2);
         } else {
             _preferredRowHeight = _rowHeight;
         }
